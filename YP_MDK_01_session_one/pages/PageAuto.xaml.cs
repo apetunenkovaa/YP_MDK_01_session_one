42e49e67afe21ca21c4af5d5aa0fba8ce4e60c09
Анастасия Петуненкова <apetunenkovaa@mail.ru>
2023-03-11 12:57:35 +0300

    Добавьте файлы проекта.

---
+++ YP_MDK_01_session_one/pages/PageAuto.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YP_MDK_01_session_one.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAuto.xaml
+    /// </summary>
+    public partial class PageAuto : Page
+    {
+        public PageAuto()
+        {
+            InitializeComponent();
+        }
+    }
+}
